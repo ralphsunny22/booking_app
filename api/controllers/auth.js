ff034944534b315a41c10e8ccc3d056cf5236fba
Ugo Sunday Raphael <ralphsunny114@gmail.com>
2023-02-06 20:27:37 +0100

    generate jwt, store in cookie

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js"
 import bcrypt from "bcryptjs"
 import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req,res,next)=>{
     try {
@@ -28,9 +29,13 @@ export const login = async (req,res,next)=>{
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
         if (!isPasswordCorrect) return next(createError(404, "Incorrect Password"))
         
+        const token = jwt.sign({ id:user._id, isAdmin:user.isAdmin }, process.env.JWT_SECRET_KEY)
+
         const { password, isAdmin, ...otherDetails } = user._doc
         //show only otherDetails
-        res.status(200).json({...otherDetails})
+        res.cookie("access_token", token, {
+            httpOnly: true
+        }).status(200).json({...otherDetails})
     } catch (err) {
         next(err)
     }
