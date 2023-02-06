836b45b9e314cd99999c1bfc051e4282e174d2f2
Ugo Sunday Raphael <ralphsunny114@gmail.com>
2023-02-06 14:51:31 +0100

    catch error in try n catch

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,62 +1,67 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
 
 const router = express.Router();
 
 //CREATE
-router.post("/", async (req,res)=>{
-    const newHotel = new Hotel(req.body)
-
+router.post("/", async (req,res,next)=>{
+    
     try {
+        const newHotel = new Hotel(req.body)
         const savedHotel = await newHotel.save()
         res.status(200).json(savedHotel)
-    } catch (error) {
-        res.status(500).json(error)
+    } catch (err) {
+        next(err)
     }
 })
 
 //UPDATE
-router.put("/:id", async (req,res)=>{
-    const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new:true })
-
+router.put("/:id", async (req,res,next)=>{
+    
     try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new:true })
         res.status(200).json(updatedHotel)
-    } catch (error) {
-        res.status(500).json(error)
+    } catch (err) {
+        next(err)
     }
 })
 
 //DELETE
-router.delete("/:id", async (req,res)=>{
-    await Hotel.findByIdAndDelete(req.params.id)
-
+router.delete("/:id", async (req,res,next)=>{
+    
     try {
+        await Hotel.findByIdAndDelete(req.params.id)
         res.status(200).json("Hotel Deleted Successfully")
-    } catch (error) {
-        res.status(500).json(error)
+    } catch (err) {
+        next(err)
     }
 })
 
 //GET
-router.get("/:id", async (req,res)=>{
-    const hotel = await Hotel.findById(req.params.id)
-
+router.get("/:id", async (req,res,next)=>{
+    
     try {
+        const hotel = await Hotel.findById(req.params.id)
         res.status(200).json(hotel)
-    } catch (error) {
-        res.status(500).json(error)
+    } catch (err) {
+        next(err)
     }
 })
 
 //GET ALL
-router.get("/", async (req,res)=>{
-    const hotels = await Hotel.find()
+router.get("/", async (req,res,next)=>{
+    // const failed = true;
+    // if (failed) return next(createError(401, "You are not authenticated"));
 
     try {
+        const hotels = await Hotel.find()
         res.status(200).json(hotels)
-    } catch (error) {
-        res.status(500).json(error)
+    } catch (err) {
+        next(err) //calling error handler in index.js
     }
 })
+
+
 
 export default router;
