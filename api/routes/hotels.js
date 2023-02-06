289902620ca293dc8d26b926d8dab5f998a24f23
Ugo Sunday Raphael <ralphsunny114@gmail.com>
2023-02-06 15:40:50 +0100

    hotel controllers, refactoring

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,67 +1,23 @@
 import express from "express";
+import { createHotel, deleteHotel, getAllHotel, getHotel, updateHotel } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
 import { createError } from "../utils/error.js";
 
 const router = express.Router();
 
 //CREATE
-router.post("/", async (req,res,next)=>{
-    
-    try {
-        const newHotel = new Hotel(req.body)
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-    } catch (err) {
-        next(err)
-    }
-})
+router.post("/", createHotel)
 
 //UPDATE
-router.put("/:id", async (req,res,next)=>{
-    
-    try {
-        const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, { $set: req.body }, { new:true })
-        res.status(200).json(updatedHotel)
-    } catch (err) {
-        next(err)
-    }
-})
+router.put("/:id", updateHotel)
 
 //DELETE
-router.delete("/:id", async (req,res,next)=>{
-    
-    try {
-        await Hotel.findByIdAndDelete(req.params.id)
-        res.status(200).json("Hotel Deleted Successfully")
-    } catch (err) {
-        next(err)
-    }
-})
+router.delete("/:id", deleteHotel)
 
 //GET
-router.get("/:id", async (req,res,next)=>{
-    
-    try {
-        const hotel = await Hotel.findById(req.params.id)
-        res.status(200).json(hotel)
-    } catch (err) {
-        next(err)
-    }
-})
+router.get("/:id", getHotel)
 
 //GET ALL
-router.get("/", async (req,res,next)=>{
-    // const failed = true;
-    // if (failed) return next(createError(401, "You are not authenticated"));
-
-    try {
-        const hotels = await Hotel.find()
-        res.status(200).json(hotels)
-    } catch (err) {
-        next(err) //calling error handler in index.js
-    }
-})
-
-
+router.get("/", getAllHotel)
 
 export default router;
